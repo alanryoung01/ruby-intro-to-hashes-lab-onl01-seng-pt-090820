4a290b8c541d26d7afa88355dcaa858157706176
Alan Young <alanryoung01@gmail.com>
2020-09-18 12:31:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,17 +15,5 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-	monopoly =  {
-  :railroads=>
-  {
-    :pieces=>
-		{},
-    :rent_in_dollars=>
-    {},
-    :names=>
-    {}
-    }
-  }
-}
-end
+
 end
